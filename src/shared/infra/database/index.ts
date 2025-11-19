c15b1dbb492bba6cae3fdfed19cdb7e8a94537ff
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-11-19 11:35:44 -0400

    chore(database): Temporarily disables the connection to PostgreSQL

--- src/shared/infra/database/index.ts
+++ src/shared/infra/database/index.ts
@@ -8,8 +8,7 @@ export async function connectDatabases() {
     await mongoose.connect(process.env.MONGODB_URI as string);
     console.log("MongoDB connected");
 
-    await prisma.$connect();
-    console.log("PostgreSQL connected");
+    console.log("PostgreSQL connection skipped for local testing.");
   } catch (error) {
     console.error("Database connection failed", error);
     process.exit(1);
