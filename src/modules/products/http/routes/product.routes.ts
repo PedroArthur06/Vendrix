179666b185b1220b09d8a241c13ae855be14981d
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-11-19 11:43:04 -0400

    feat(routes): Adds new routes that will be used by the admin/seller

--- src/modules/products/http/routes/product.routes.ts
+++ src/modules/products/http/routes/product.routes.ts
@@ -2,21 +2,33 @@ import { Router } from "express";
 import { ProductController } from "../controllers/product.controller";
 import { validateCreateProduct } from "../middlewares/validation.middleware";
 import { authenticateToken } from "../../../../shared/infra/http/middlewares/auth.middleware";
+import { authorizeRoles } from "../../../../shared/infra/http/middlewares/authorization.middleware";
 
 const router = Router();
 const productController = new ProductController();
 
-router.get(
-  "/products",
-  authenticateToken,
-  productController.findAll.bind(productController)
-);
+router.get("/products", productController.findAll.bind(productController));
 
 router.post(
   "/products",
   authenticateToken,
+  authorizeRoles(["admin", "supplier"]),
   validateCreateProduct,
   productController.create.bind(productController)
+);
+
+router.patch(
+  "/products/:id",
+  authenticateToken,
+  authorizeRoles(["admin", "supplier"]),
+  productController.update.bind(productController)
+);
+
+router.delete(
+  "/products/:id",
+  authenticateToken,
+  authorizeRoles(["admin", "supplier"]),
+  productController.delete.bind(productController)
 );
 
 export default router;
