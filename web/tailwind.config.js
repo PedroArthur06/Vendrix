b74ed304d8a9fff7014dab2ea54a34e56fcf307f
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-11-20 08:26:09 -0400

    chore: Adds the app's theme settings and colors.

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -10,16 +10,11 @@ export default {
         sm: "calc(var(--radius) - 4px)",
       },
       colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
@@ -27,6 +22,10 @@ export default {
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
         muted: {
           DEFAULT: "hsl(var(--muted))",
@@ -36,20 +35,32 @@ export default {
           DEFAULT: "hsl(var(--accent))",
           foreground: "hsl(var(--accent-foreground))",
         },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        chart: {
-          1: "hsl(var(--chart-1))",
-          2: "hsl(var(--chart-2))",
-          3: "hsl(var(--chart-3))",
-          4: "hsl(var(--chart-4))",
-          5: "hsl(var(--chart-5))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
+
+        neumo: {
+          bg: "#212121",
+          text: "#e0e0e0",
+          highlight: "#3d3d3d",
+          shadow: "#0d0d0d",
+        },
+        brand: {
+          DEFAULT: "#43BBA8",
+          light: "#9AD490",
+          dark: "#339763",
+          deep: "#1F282E",
+        },
+      },
+      boxShadow: {
+        "neumo-flat": "8px 8px 16px #151515, -8px -8px 16px #2d2d2d",
+        "neumo-pressed":
+          "inset 6px 6px 12px #151515, inset -6px -6px 12px #2d2d2d",
       },
     },
   },
