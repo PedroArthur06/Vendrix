ae49009fbb58b4b707c8370aca22bd26b0115232
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-11-20 08:24:02 -0400

    chore: It adds the login and registration routes, and sets the home screen as the root route.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,31 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SignIn } from "./pages/auth/sign-in";
+import { SignUp } from "./pages/auth/sign-up";
+import { Toaster } from "@/components/ui/sonner";
+import { Home } from "./pages/home";
+import { AppLayout } from "./pages/_layouts/app-layout";
 
 const queryClient = new QueryClient();
-
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/login" element={<SignIn />} />
+          <Route path="/" element={<Home />} />
 
-          <Route path="/dashboard" element={<h1>Dashboard (Área Logada)</h1>} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/register" element={<SignUp />} />
+
+          <Route path="/admin" element={<AppLayout />}>
+            {" "}
+            <Route index element={<Navigate to="/admin/dashboard" replace />} />
+            <Route path="dashboard" element={<h1>Dashboard</h1>} />
+            <Route path="products" element={<h1>Produtos</h1>} />
+          </Route>
         </Routes>
       </BrowserRouter>
+      <Toaster richColors closeButton theme="dark" />
     </QueryClientProvider>
   );
 }
