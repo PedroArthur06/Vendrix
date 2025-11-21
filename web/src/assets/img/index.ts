0cfe4697581be42adfe36874a353ed01a14a69a1
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-11-21 14:18:03 -0400

    chore: exports all photos in a single constant

---
+++ web/src/assets/img/index.ts
@@ -0,0 +1,11 @@
+import nikeFuture from "./nike.jpg";
+import adidasUltra from "./adidas-ultra.jpg";
+import yeezyGreen from "./yeezy-green.jpeg";
+import heroSneaker from "./tenis-futuro.png";
+
+export const Products = {
+  nike: nikeFuture,
+  adidas: adidasUltra,
+  yeezy: yeezyGreen,
+  hero: heroSneaker,
+};
